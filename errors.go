0a389d17fd60a9436cc006c89dec7bf40d4937bf
__Luong <dot@luongmb.local>
2024-10-23 13:51:53 +0700

    mongo lock

--- errors.go
+++ errors.go
@@ -1,39 +1,62 @@
 package golock
 
-type ErrLockAlreadyExists struct {
+import "errors"
+
+var (
+	ErrLockAlreadyExists   error = &errLockAlreadyExists{}
+	ErrUnsupportedLockType error = &unsupportedLockType{}
+	ErrLockNotFound        error = &errLockNotFound{}
+)
+
+type errLockAlreadyExists struct {
 	name string
 }
 
-func (e *ErrLockAlreadyExists) Error() string {
+func (e *errLockAlreadyExists) Is(target error) bool {
+	var ee *errLockAlreadyExists
+	return errors.As(target, &ee)
+}
+
+func (e *errLockAlreadyExists) Error() string {
 	return "lock already exists: " + e.name
 }
 
-func NewErrLockAlreadyExists(name string) *ErrLockAlreadyExists {
-	return &ErrLockAlreadyExists{name: name}
+func NewErrLockAlreadyExists(name string) error {
+	return &errLockAlreadyExists{name: name}
 }
 
-var _ error = (*ErrLockAlreadyExists)(nil)
+var _ error = (*errLockAlreadyExists)(nil)
 
-type UnsupportedLockType struct {
+type unsupportedLockType struct {
 	lockType string
 }
 
-func (e *UnsupportedLockType) Error() string {
+func (e *unsupportedLockType) Is(target error) bool {
+	var ee *unsupportedLockType
+	return errors.As(target, &ee)
+}
+
+func (e *unsupportedLockType) Error() string {
 	return "supported only lock type: " + e.lockType
 }
 
-func NewErrUnsupportedLockType(lockType string) *UnsupportedLockType {
-	return &UnsupportedLockType{lockType: lockType}
+func NewErrUnsupportedLockType(lockType string) error {
+	return &unsupportedLockType{lockType: lockType}
 }
 
-type ErrLockNotFound struct {
+type errLockNotFound struct {
 	name string
 }
 
-func (e *ErrLockNotFound) Error() string {
+func (e *errLockNotFound) Is(target error) bool {
+	var ee *errLockNotFound
+	return errors.As(target, &ee)
+}
+
+func (e *errLockNotFound) Error() string {
 	return "lock not found: " + e.name
 }
 
-func NewErrLockNotFound(name string) *ErrLockNotFound {
-	return &ErrLockNotFound{name: name}
+func NewErrLockNotFound(name string) error {
+	return &errLockNotFound{name: name}
 }
